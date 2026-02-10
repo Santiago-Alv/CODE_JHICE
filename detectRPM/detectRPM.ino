f2f0995fb954d308e0316e278345f0625ebedd24
Santiago-Alv <214873111+Santiago-Alv@users.noreply.github.com>
2026-02-09 17:36:26 -0800

    feat[detectRPM.ino]: Añado codigo para detectar las RPM del motr

---
+++ detectRPM/detectRPM.ino
@@ -0,0 +1,66 @@
+// Definir los pines
+const int escPin = 4;
+// Motor conectado en  D4
+const int pinSensor = 14;  
+// Sensor conectado en D14
+
+// Configuración del Motor
+const int freq = 50;
+const int resolution = 12;
+const int MIN_PWM = 205;   
+// Parado del motor y el armado
+const int MAX_PWM = 410;   
+// Máxima potencia del motor
+
+// Configuración del sensor
+volatile long pulsos = 0;
+unsigned long tiempoAnterior = 0;
+const int ranurasDisco = 20;
+// Ranuras del disco fisico
+
+// Función para contar los pulsos (Interrupción)
+void IRAM_ATTR contarPulsos() {
+  pulsos++;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // 1. Configuracion del sensor
+  pinMode(pinSensor, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinSensor), contarPulsos, FALLING); 
+
+  // 2. Configurar el motor
+  ledcAttach(escPin, freq, resolution);
+
+  // 3. Proceso de armado
+  Serial.println("Armando ESC... (5 segundos)");
+  ledcWrite(escPin, MIN_PWM); 
+  delay(5000); 
+  Serial.println("¡Motor listo y Sensor activo!");
+}
+
+void loop() {
+  // Prueba de aceleracion 
+  int velocidadDePrueba = MIN_PWM + 40; 
+  ledcWrite(escPin, velocidadDePrueba);
+
+  // CÁLCULO DE RPM (Cada 1 segundo)
+  unsigned long tiempoActual = millis();
+  if (tiempoActual - tiempoAnterior >= 1000) {
+    
+    noInterrupts(); // Pausa breve para leer la variable sin errores
+    long pulsosCopia = pulsos;
+    pulsos = 0;
+    interrupts(); 
+
+    tiempoAnterior = tiempoActual;
+    
+    // Fórmula = (pulsos / ranuras del disco) x 60 segundos
+    float rpm = ((float)pulsosCopia / ranurasDisco) * 60.0;
+    
+    Serial.print("Pulsos: "); Serial.print(pulsosCopia);
+    Serial.print(" | Velocidad: "); Serial.print(rpm);
+    Serial.println(" RPM");
+  }
+}
